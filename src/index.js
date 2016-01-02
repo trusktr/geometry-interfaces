cbc3d01ebde53c8ff7c7dfdf2c0d4fd238a52b8d
Joseph Orbegoso Pea <joe@trusktr.io>
2016-01-01 22:50:53 -0800

    Don't check for webpack in index.js, just apply the polyfill. This way we can `import 'dommetry'` to apply the polyfill on demand.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import DOMMatrix from './DOMMatrix'
 import DOMMatrixReadOnly from './DOMMatrixReadOnly'
 
-// hack needed for global build:
-if (typeof __webpack_require__ != 'undefined') {
-    let _global = null
+let _global = null
 
-    // browser
-    if (typeof window != 'undefined') {
-        _global = window
-    }
-    else if (typeof global != 'undefined') {
-        _global = global
-    }
+// browser
+if (typeof window != 'undefined') {
+    console.log('detected window')
+    _global = window
+}
+else if (typeof global != 'undefined') {
+    _global = global
+}
 
-    if (_global) {
-        _global.DOMMatrix = DOMMatrix
-        _global.DOMMatrixReadOnly = DOMMatrixReadOnly
-    }
+if (_global) {
+    _global.DOMMatrix = DOMMatrix
+    _global.DOMMatrixReadOnly = DOMMatrixReadOnly
 }
 
 export {
