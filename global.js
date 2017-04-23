3aac80fdd036c7349393c3c044eb296cbbac264a
Joe Pea <joe@trusktr.io>
2017-04-22 17:45:58 -0700

    v1.0.5

---
+++ global.js
@@ -0,0 +1,85 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["GeometryInterfaces"] = factory();
+	else
+		root["GeometryInterfaces"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+	/* WEBPACK VAR INJECTION */(function(global) {import DOMMatrix from './DOMMatrix'
+	import DOMMatrixReadOnly from './DOMMatrixReadOnly'
+
+	let _global = null
+
+	// browser
+	if (typeof window != 'undefined') {
+	    _global = window
+	}
+	else if (typeof global != 'undefined') {
+	    _global = global
+	}
+
+	if (_global) {
+	    _global.DOMMatrix = DOMMatrix
+	    _global.DOMMatrixReadOnly = DOMMatrixReadOnly
+	}
+
+	export {
+	    DOMMatrix,
+	    DOMMatrixReadOnly,
+	}
+
+	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
+
+/***/ })
+/******/ ])
+});
+;
