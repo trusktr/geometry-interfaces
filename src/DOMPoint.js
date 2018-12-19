4fc162fe1539f3951621d6b99577159fded49524
Paul Capron <paul@fragara.com>
2018-12-19 18:57:01 +0100

    Fix DOMPoint{ReadOnly} read-write perms cleanly
    Related commit: aaa448c08eb0c457c69c6c29c6277b0b8998aac9.
    See also https://github.com/trusktr/geometry-interfaces/pull/8#issuecomment-444220161

--- src/DOMPoint.js
+++ src/DOMPoint.js
@@ -25,21 +25,12 @@ class DOMPointReadOnly {
         _(this).y = (y !== undefined) ? Number(y) : 0
         _(this).z = (z !== undefined) ? Number(z) : 0
         _(this).w = (w !== undefined) ? Number(w) : 1
-
-        if (this.constructor === DOMPointReadOnly) {
-            Object.freeze(_(this))
-        }
     }
 
     get x() { return _(this).x }
     get y() { return _(this).y }
     get z() { return _(this).z }
     get w() { return _(this).w }
-
-    set x(value) { _(this).x = Number(value) }
-    set y(value) { _(this).y = Number(value) }
-    set z(value) { _(this).z = Number(value) }
-    set w(value) { _(this).w = Number(value) }
 
     matrixTransform(matrix) {
         let result = new this.constructor(this)
@@ -60,6 +51,16 @@ class DOMPointReadOnly {
 }
 
 export
-class DOMPoint extends DOMPointReadOnly {}
+class DOMPoint extends DOMPointReadOnly {
+    get x() { return super.x }
+    get y() { return super.y }
+    get z() { return super.z }
+    get w() { return super.w }
+
+    set x(value) { _(this).x = Number(value) }
+    set y(value) { _(this).y = Number(value) }
+    set z(value) { _(this).z = Number(value) }
+    set w(value) { _(this).w = Number(value) }
+}
 
 export default DOMPoint
